1933cb3a62190c7949f211f2b4694e3da33090a7
n8fr8 <nathan@freitas.net>
2013-11-24 06:50:46 -0500

    adding default setup script

---
+++ setup-ant.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+target="android-18"
+
+for f in `find external/ -name project.properties`; do
+projectdir=`dirname $f`
+    echo "Updating ant setup in $projectdir:"
+    android update lib-project -p $projectdir -t $target
+done
+android update project -p . --subprojects -t $target --name orwebng
+
